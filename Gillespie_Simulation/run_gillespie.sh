0a88892906fff0052a6ae61eeaca43c14c2020fb
jordanbchilds <74244328+jordanbchilds@users.noreply.github.com>
2021-08-24 13:24:02 +0100

    run_gillespie remove -u

--- Gillespie_Simulation/run_gillespie.sh
+++ Gillespie_Simulation/run_gillespie.sh
@@ -1,8 +1,8 @@
-#!/bin/sh -u
+#!/bin/sh
 
 counter=1
 while [ $counter -le $1 ]
 do
-	nohup Rscript -u single_gillespied.R "$counter" &
+	nohup Rscript single_gillespied.R "$counter" &
 	counter=$(( counter+1 ))
 done
